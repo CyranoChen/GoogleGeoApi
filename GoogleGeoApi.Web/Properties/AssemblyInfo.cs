25e0611050005390b075ce168a5250619a308a9a
Cyrano <cyrano@arsenalcn.com>
2018-02-16 16:04:51 +0800

    Release final version

--- GoogleGeoApi.Web/Properties/AssemblyInfo.cs
+++ GoogleGeoApi.Web/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GoogleGeoApi.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GoogleGeoApi.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("SAP SMB Innvocation Lab")]
+[assembly: AssemblyProduct("GoogleGeoApi")]
+[assembly: AssemblyCopyright("©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0")]
